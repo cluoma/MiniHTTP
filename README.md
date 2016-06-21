a1fce5fbfbdebb9efe44ca3d2414b86e9841764d
cluoma <themjolnir999@gmail.com>
2016-06-21 18:49:04 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#MiniHTTP
+
+A lightweight HTTP webserver purpose-built for small systems such as the Raspberry Pi.
---
+++ src/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MiniHTTP
+//
+//  Created by Colin Luoma on 2016-06-21.
+//  Copyright (c) 2016 Colin Luoma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
